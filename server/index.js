7296aa075f3d6ecb5638238fbf1e79cca2fbb211
singhdivyansh0009 <102811377+singhdivyansh0009@users.noreply.github.com>
2023-12-19 22:49:25 +0530

    upload

---
+++ server/index.js
@@ -0,0 +1,113 @@
+import express from "express";
+import cors from "cors";
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import path from 'path';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const mongoURI = "mongodb+srv://divyanshkrsingh200:Aman9534328052@cluster0.jo8nygw.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// creating a collection
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+// creating a user model for the schema
+const User = mongoose.model('User', userSchema);
+
+// Routes
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).send({ message: "Missing required fields" });
+    }
+
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      const passwordMatch = await bcrypt.compare(password, existingUser.password);
+
+      if (passwordMatch) {
+        console.log("Login successful");
+        res.send({ message: "Login successful", user: existingUser });
+      } else {
+        console.log("Incorrect password");
+        res.send({ message: "Incorrect password", user: 0 });
+      }
+    } else {
+      console.log("User not found");
+      res.send({ message: "User not found", user: 0 });
+    }
+  } catch (error) {
+    console.error(error);
+    res.send({ message: "Internal Server Error" });
+  }
+});
+
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check for missing fields
+    if (!name || !email || !password) {
+      return res.status(400).send({ message: "Missing required fields" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      console.log("Already present")
+      return res.send({ message: "User already registered" });
+    }
+
+    // Create a new user with the hashed password
+    const newUser = new User({
+      name,
+      email,
+      password:hashedPassword
+    });
+
+    // Save the user to the database
+    await newUser.save();
+    
+    console.log(newUser);
+    res.send({ message: "Successfully registered" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+// static file 
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
